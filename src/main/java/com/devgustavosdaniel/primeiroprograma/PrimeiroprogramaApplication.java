4cf0d7edc41df3d34e84d2faf0dcbd28898fee34
GustavoGSD <gustavosdaniel@hotmail.com>
2025-01-20 20:58:58 -0300

    Projeto criado

---
+++ src/main/java/com/devgustavosdaniel/primeiroprograma/PrimeiroprogramaApplication.java
@@ -0,0 +1,13 @@
+package com.devgustavosdaniel.primeiroprograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprogramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprogramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devgustavosdaniel/primeiroprograma/PrimeiroprogramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgustavosdaniel.primeiroprograma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroprogramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
