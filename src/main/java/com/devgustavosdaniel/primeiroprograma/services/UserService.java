2afb538d733394463a7096f4efbe03fbf24a7a1d
GustavoGSD <gustavosdaniel@hotmail.com>
2025-01-21 22:17:50 -0300

    Servoce layer, component registration

---
+++ src/main/java/com/devgustavosdaniel/primeiroprograma/services/UserService.java
@@ -0,0 +1,40 @@
+package com.devgustavosdaniel.primeiroprograma.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devgustavosdaniel.primeiroprograma.entities.User;
+import com.devgustavosdaniel.primeiroprograma.repositores.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
