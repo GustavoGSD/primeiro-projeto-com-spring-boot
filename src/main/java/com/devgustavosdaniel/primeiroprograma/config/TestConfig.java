0ce0ec00cc6c240f57e5e4ca2c5cf8081e12d20f
GustavoGSD <gustavosdaniel@hotmail.com>
2025-01-22 23:08:33 -0300

    Category

--- src/main/java/com/devgustavosdaniel/primeiroprograma/config/TestConfig.java
+++ src/main/java/com/devgustavosdaniel/primeiroprograma/config/TestConfig.java
@@ -1,17 +1,19 @@
 package com.devgustavosdaniel.primeiroprograma.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.devgustavosdaniel.primeiroprograma.entities.Category;
 import com.devgustavosdaniel.primeiroprograma.entities.Order;
 import com.devgustavosdaniel.primeiroprograma.entities.User;
 import com.devgustavosdaniel.primeiroprograma.entities.enums.OrderStatus;
+import com.devgustavosdaniel.primeiroprograma.repositores.CategoryRepository;
 import com.devgustavosdaniel.primeiroprograma.repositores.OrderRepository;
 import com.devgustavosdaniel.primeiroprograma.repositores.UserRepository;
 
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,8 +41,15 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,  u2); 
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED,  u1); 
 		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 }
