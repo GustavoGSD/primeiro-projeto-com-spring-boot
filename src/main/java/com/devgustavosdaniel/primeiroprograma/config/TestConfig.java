cebecb12e19ed04f8159a801bcc07be258b67a58
GustavoGSD <gustavosdaniel@hotmail.com>
2025-01-25 11:03:09 -0300

    Payment, one-to-one association

--- src/main/java/com/devgustavosdaniel/primeiroprograma/config/TestConfig.java
+++ src/main/java/com/devgustavosdaniel/primeiroprograma/config/TestConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Profile;
 import com.devgustavosdaniel.primeiroprograma.entities.Category;
 import com.devgustavosdaniel.primeiroprograma.entities.Order;
 import com.devgustavosdaniel.primeiroprograma.entities.OrderItem;
+import com.devgustavosdaniel.primeiroprograma.entities.Payment;
 import com.devgustavosdaniel.primeiroprograma.entities.Product;
 import com.devgustavosdaniel.primeiroprograma.entities.User;
 import com.devgustavosdaniel.primeiroprograma.entities.enums.OrderStatus;
@@ -86,5 +87,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
